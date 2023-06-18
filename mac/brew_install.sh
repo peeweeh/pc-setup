b9bf15dccc0d3e2cb9700637a8fec4a6d792aa55
Paul <me@paulhidalgo.net>
2023-06-18 12:57:36 +0800

    Initial commit

---
+++ mac/brew_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+brew update
+brew upgrade
+brew install git
+brew install --cask 1password
+brew install aws-nuke
+brew install awscli
+brew install --cask brave-browser
+brew install --cask slack
+brew install --cask vlc
+brew install --cask ferdium
+brew install --cask google-chrome
+brew install --cask google-drive
+brew install --cask microsoft-teams
+brew install --cask nordvpn
+brew install --cask steam
+brew install --cask visual-studio-code
+brew install --cask zoom
+brew install --cask microsoft-office
+brew install --cask amazon-chime
+brew install --cask amazon-workspaces
+brew install --cask microsoft-remote-desktop
+brew install --cask istat-menus
+brew install --cask rectangle
+brew install --cask discord
+brew install --cask adobe-creative-cloud
+brew install --cask fig
+brew install --cask devtoys
+brew install --cask postman
+brew install switchaudio-osx
+
+#This code is a Bash script that installs various software packages and applications using Homebrew on a macOS system. The script updates Homebrew, upgrades existing packages, and installs new packages such as Git, 1Password, AWS CLI, Slack, VLC, Google Chrome, Microsoft Office, Adobe Creative Cloud, and more. The script also installs some command-line tools like SwitchAudio-OSX. The goal of this script is to automate the installation of commonly used software and tools on a macOS system.
---
+++ mac/mac_install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Auto hide the dock
+defaults write com.apple.dock autohide -bool true
+
+# Remove all items from the dock
+defaults write com.apple.dock persistent-apps -array
+
+# Resize the dock to 50%
+defaults write com.apple.dock tilesize -int 36
+
+# Restart the dock for changes to take effect
+killall Dock
+
+#!/bin/bash
+defaults delete com.apple.sidebarlists favoriteitems
+
+# Show only Home, AirDrop, Downloads, and iCloud
+defaults write com.apple.sidebarlists favoriteitems -array-add "com.apple.HomeDirectory"
+defaults write com.apple.sidebarlists favoriteitems -array-add "com.apple.NetworkBrowser"
+defaults write com.apple.sidebarlists favoriteitems -array-add "com.apple.Locales"
+defaults write com.apple.sidebarlists favoriteitems -array-add "com.apple.mac.iCloud"
+defaults write com.apple.sidebarlists favoriteitems -array-add "com.apple.Downloads"
+
+osascript -e 'tell application "Finder" to set target of Finder window 1 to (path to home folder)'
+
+defaults write com.apple.finder ShowRecentTags -bool false
+
+killall Finder
+
+# Define the URL of the Nord.terminal file
+url="https://raw.githubusercontent.com/nordtheme/terminal-app/develop/src/xml/Nord.terminal"
+
+# Define the local file path
+file="$HOME/Downloads/Nord.terminal"
+
+# Download the Nord.terminal file
+curl -L $url -o $file
+
+# Open the Nord.terminal file, which will install the theme in Terminal
+open $file
+
