54544fed9aca206d9f723d7304f04d442d966445
Paul Hidalgo <me@paulhidalgo.net>
2023-07-09 16:45:41 +0800

    added docker

--- mac/brew_install.sh
+++ mac/brew_install.sh
@@ -3,24 +3,26 @@ brew update
 brew upgrade
 brew install git
 brew install --cask 1password
+brew install --cask google-chrome
+brew install --cask visual-studio-code
 brew install aws-nuke
 brew install awscli
 brew install --cask brave-browser
 brew install --cask slack
 brew install --cask vlc
 brew install --cask ferdium
-brew install --cask google-chrome
+
 brew install --cask google-drive
 brew install --cask microsoft-teams
 brew install --cask nordvpn
-brew install --cask steam
-brew install --cask visual-studio-code
+
+
 brew install --cask zoom
 brew install --cask microsoft-office
 brew install --cask amazon-chime
 brew install --cask amazon-workspaces
 brew install --cask microsoft-remote-desktop
-brew install --cask istat-menus
+
 brew install --cask rectangle
 brew install --cask discord
 brew install --cask adobe-creative-cloud
@@ -34,4 +36,8 @@ brew install romkatv/powerlevel10k/powerlevel10k
 brew install bat
 brew install fzf
 brew install exa
+
+brew install --cask docker
+brew install --cask steam
+
 #This code is a Bash script that installs various software packages and applications using Homebrew on a macOS system. The script updates Homebrew, upgrades existing packages, and installs new packages such as Git, 1Password, AWS CLI, Slack, VLC, Google Chrome, Microsoft Office, Adobe Creative Cloud, and more. The script also installs some command-line tools like SwitchAudio-OSX. The goal of this script is to automate the installation of commonly used software and tools on a macOS system.
