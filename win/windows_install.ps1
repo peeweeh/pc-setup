a38e039c1461550a8a1dc0386fa912d910920d5e
Paul Hidalgo <me@paulhidalgo.net>
2023-07-15 16:44:01 +0800

    prettier

--- win/windows_install.ps1
+++ win/windows_install.ps1
@@ -1,14 +1,38 @@
-echo "================ Installing 1Password"
+# ASCII Art and Colors
+Write-Host -ForegroundColor Green "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Installing Applications ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
+# ----------------------------------------------
+# 1Password
+# ----------------------------------------------
+Write-Host "`n`t`t" -NoNewline -ForegroundColor Red; Write-Host "Installing 1Password" -ForegroundColor Yellow 
 Invoke-WebRequest -Uri "https://downloads.1password.com/win/1PasswordSetup-latest.exe" -OutFile "https://downloads.1password.com/win/1PasswordSetup-latest.exe"
 Start-Process -FilePath "1PasswordInstaller.exe" -ArgumentList "/S"
 
-echo "================ Installing Choco"
-
+# ----------------------------------------------
+# Chocolatey
+# ----------------------------------------------
+Write-Host "`n`t`t" -NoNewline -ForegroundColor Red; Write-Host "Installing Chocolatey" -ForegroundColor Yellow
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install -y GoogleChrome nodejs powertoys steam visualstudiocode vlc vscode winrar brave ferdium slack amazon-chime amazon-workspaces plex
 
-echo "================ Oh My Posh Choco"
+# ----------------------------------------------
+# Applications List
+# ----------------------------------------------
+$appList = "git", "GoogleChrome", "nodejs", "powertoys", "visualstudiocode", 
+"vlc", "vscode", "winrar", "brave", "ferdium", "slack", "amazon-chime", "amazon-workspaces", 
+"googledrive", "plex", "nerd-fonts-firacode", "nerd-fonts-firamono", "postman", "fzf", "bat", 
+"nordvpn", "steam"
+
+# ----------------------------------------------
+# Installing Applications via Chocolatey
+# ----------------------------------------------
+Write-Host "`n`t`t" -NoNewline -ForegroundColor Red; Write-Host "Installing applications via Chocolatey" -ForegroundColor Yellow
+$appList | % { choco install -y $_ }
+
+
+# ----------------------------------------------
+# Oh My Posh
+# ----------------------------------------------
+Write-Host "`n`t`t" -NoNewline -ForegroundColor Red; Write-Host "Installing Oh My Posh" -ForegroundColor Yellow
 winget install JanDeDobbeleer.OhMyPosh -s winget
 (Get-Command oh-my-posh).Source
 New-Item -Path $PROFILE -Type File -Force
@@ -16,7 +40,25 @@ $filePath = $PROFILE
 $lineToAdd = "oh-my-posh init pwsh | Invoke-Expression"
 Add-Content -Path $filePath -Value $lineToAdd
 
-echo "================ Installing WSL"
-wsl --install -d Ubuntu; Restart-Computer
+# ----------------------------------------------
+# WSL - Ubuntu
+# ----------------------------------------------
+Write-Host "`n`t`t" -NoNewline -ForegroundColor Red; Write-Host "Installing WSL - Ubuntu" -ForegroundColor Yellow
+wsl --install -d Ubuntu;
 
 
+# ----------------------------------------------
+# Docker Desktop
+# ----------------------------------------------
+Write-Host "`n`t`t" -NoNewline -ForegroundColor Red; Write-Host "Installing Docker Desktop" -ForegroundColor Yellow
+
+$url = "https://download.docker.com/win/stable/Docker%20Desktop%20Installer.exe"
+Invoke-WebRequest -Uri $url -OutFile "$env:TEMP\DockerDesktopInstaller.exe"
+
+# Install Docker Desktop
+Start-Process -FilePath "$env:TEMP\DockerDesktopInstaller.exe" -ArgumentList "/S" -Wait
+Start-Service Docker
+
+Write-Host "`n`t`t" -NoNewline -ForegroundColor Red; Write-Host "Installation Complete!" -ForegroundColor Yellow
+
+Restart-Computer
